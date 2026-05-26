f86d61be34b46aec51bfeaeddb2d76c193373a7a
Dpendragon <96967984+Dpendragon@users.noreply.github.com>
2026-05-26 19:11:44 +0700

    feat<core>: add Controller + ConnectDB

---
+++ app/core/Controller.php
@@ -0,0 +1,15 @@
+<?php
+class Controller
+{
+  public function model($model)
+  {
+    require_once "../app/models/" . $model . ".php";
+    return new $model();
+  }
+
+  public function view($viewname, $data = [])
+  {
+    extract($data);
+    require_once "../app/views/" . $viewname . ".php";
+  }
+}
