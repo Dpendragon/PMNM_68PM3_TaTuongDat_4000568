6ba7835aaf93fd14e4065906a57f9a947a61b004
Dpendragon <96967984+Dpendragon@users.noreply.github.com>
2026-05-26 19:14:39 +0700

    feat: Hiển thị danh sách sinh viên

--- app/controllers/sinhvien.php
+++ app/controllers/sinhvien.php
@@ -1,10 +1,14 @@
 <?php
-class sinhvien
+require_once "../app/core/controller.php";
+class sinhvien extends Controller
 {
   public function index()
   {
+    $sinhvienModel = $this->model('sinhvienModel');
+    $sinhviens = $sinhvienModel->getAllSinhVien();
     // Trả về View
-    require_once "../app/views/sinhvien/index.php";
+    //require_once "../app/views/sinhvien/index.php";
+    $this->view('sinhvien/index', ['sinhviens' => $sinhviens]);
   }
 
   public function create()
